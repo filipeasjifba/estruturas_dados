9a98490b2eecbcf9539a542084ff4ddd4d31b0f5
Filipe Alves <202421190039@ifba.edu.br>
2025-09-08 12:07:24 -0300

    Update Questao8.java

--- Lista de exercícios/Questao8.java
+++ Lista de exercícios/Questao8.java
@@ -11,11 +11,11 @@ public class Questao8 {
         for (int i = 0; i < 5; i++) {
             System.out.print("digite a idade da " + (i+1) +"º pessoa: ");
             idade[i] = scanner.nextInt();
-            System.out.print("digite a altura da" + (i+1) + "º pessoa: ");
+            System.out.print("digite a altura da " + (i+1) + "º pessoa: ");
             altura[i] = scanner.nextDouble();
         }
 
-        for (int i = 5; i >= 0; i--) {
+        for (int i = 4; i >= 0; i--) {
             System.out.println((i+1) + "º pessoa: " + idade[i] + " anos / " + altura[i]);
         }
     }
