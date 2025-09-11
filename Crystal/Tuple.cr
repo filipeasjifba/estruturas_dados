95375ad5dd5ef18d2262da9c276ac9446cd7a467
Filipe Alves <202421190039@ifba.edu.br>
2025-09-11 12:06:56 -0300

    Update Tuple.cr

--- Crystal/Tuple.cr
+++ Crystal/Tuple.cr
@@ -1,4 +1,4 @@
-# Classe TuplaMetodos
+# Tupla (tuple)
 
 class TuplaMetodos(T)
 
@@ -23,18 +23,18 @@ class TuplaMetodos(T)
     def [](index)
         @tuple[index]
     end
-
-    # Não há tantos métodos pois a tupla é imutável
 end
 
-#Classe main
-
 class Main
+    vitoria_da_conquista = TuplaMetodos.new({-14.8648, -40.8369})
+    anage = TuplaMetodos.new({"-14.5995", "-41.1337"})
+    barra_do_choça = TuplaMetodos.new({-14.8641,-40.5591})
+    planalto = TuplaMetodos.new({-14.6604, -40.4789})
 
-    minha_tupla = TuplaMetodos.new({"Claudio", 18, "Rodolfo", 30})
-    puts "A tupla está vazia? #{minha_tupla.empty?}"
-    puts "A tupla está cheia? #{minha_tupla.full?}"
-    puts "o tamanho da tupla é: #{minha_tupla.size} objetos"
-    p "Exemplo: A aula do professor #{minha_tupla[0]} #{minha_tupla[2]} acontece na segunda às #{minha_tupla[1]}:#{minha_tupla[3]}"
-
+    puts "-----Localizações-----"
+    puts "Vitória da Conquista: latitude #{vitoria_da_conquista[0]}, longitude #{vitoria_da_conquista[1]}"
+    puts "Anagé: latitude #{anage[0]}, longitude #{anage[1]}"
+    puts "Barra do choça: latitude #{barra_do_choça[0]}, longitude #{barra_do_choça[1]}"
+    puts "Planalto: latitude #{planalto[0]}, longitude #{planalto[1]}"
+    
 end
