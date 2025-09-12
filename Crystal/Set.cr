e5109b049792cc8792c2e69ba1520a3f8595bf4d
Filipe Alves <202421190039@ifba.edu.br>
2025-09-12 10:58:09 -0300

    Update Set.cr

--- Crystal/Set.cr
+++ Crystal/Set.cr
@@ -3,11 +3,15 @@ require "set"
 s = Set{1,2,3}
 
 class Main
+    puts "-----Lista de convidados-----"
     lista_de_convidados = Set{"Leo", "Aline", "Cláudio", "Alexandro", "Leo", "Liojes", "Viviane", "Cláudio"}
     puts lista_de_convidados
-
+    puts "Na estrutura Set, as repetições são ignoradas"
+    
+    puts "\n-----Lista de emails-----"
     emails_bd = Set{"a@gmail.com", "b@gmail.com", "c@gmail.com", "c@gmail.com", "d@gmail.com"}
     emails_bd.add("b@gmail.com")
     emails_bd.delete("c@gmail.com")
     puts emails_bd
+    puts "Mesmo adicionando uma cópia, a estrutura Set ignora a adição"
 end
