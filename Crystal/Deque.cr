1d2760358be8bc219a0ffd100372167bbc40de16
Filipe Alves <202421190039@ifba.edu.br>
2025-09-16 11:11:14 -0300

    Adição de interface para Deque.cr

--- Crystal/Deque.cr
+++ Crystal/Deque.cr
@@ -1,31 +1,75 @@
+# Por não ser nativo, tivemos que importar uma biblioteca
 require "deque"
 
 class Main
-    puts "-----navegação------"
-    nav = Deque(String).new
-    
-    nav.unshift("google.com")
-    nav.unshift("youtube.com")
-    nav.unshift("gov.br")
-    
-    puts "Histórico: #{nav}"
-    puts "Mais recente: #{nav.first}"
-    puts "Mais antigo: #{nav.last}"
-    
-    puts "voltando uma página e acessando outra"
-    nav.shift
-    nav.unshift("github.com")
-    puts nav
-        
-    puts "\n-----fila única com prioridade------"
-    fila = Deque(String).new
+    pedidos = Deque(String).new # Lembrando que o deque só recebe um tipo
+    loop do
+      puts "\n-----SISTEMA DA LANCHONETE DO IFBA-----"
+        puts "1 - Novo pedido (push)"
+        puts "2 - Pedido prioritário (unshift)"
+        puts "3 - Atender pedido (shift)"
+        puts "4 - Cancelar último pedido (pop)"
+        puts "5 - Ver o primeiro pedido (first)"
+        puts "6 - Ver último pedido (last)"
+        puts "7 - Ver quantos pedidos estão na fila (size)"
+        puts "8 - Limpar todos os pedidos (clear)"
+        puts "9 - Mostrar fila completa (to_a)"
+        puts "0 - Sair"
+        print "Escolha uma opção:" # usamos print só nesse aqui, porque puts quebra linha
+        opcao = gets.to_s.chomp
 
-    fila.push("Ana")
-    fila.push("José")
-    fila.push("Maria")
-    puts fila
-    puts "Chegou uma gestante (Fernanda)"
-    fila.unshift("Fernanda")
-    puts fila
-
+        #Fizemos verificações para evitar exceções
+        case opcao
+        when "1"
+            print "Digite o pedido: "
+            p = gets.to_s.chomp
+            pedidos.push(p)
+            puts "Pedido adicionado na fila: #{p}"
+        when "2"
+            print "Digite o pedido prioritário: "
+            p = gets.to_s.chomp
+            pedidos.unshift(p)
+            puts "Pedido prioritário adicionado no início da fila: #{p}"
+        when "3"
+            if pedidos.empty?
+                puts "Nenhum pedido para atender"
+            else
+                atendido = pedidos.shift
+                puts "Pedido atendido: #{atendido}"
+            end
+        when "4"
+            if pedidos.empty?
+                puts "Nenhum pedido para cancelar"
+            else
+                cancelado = pedidos.pop
+                puts "pedido cancelado: #{cancelado}"
+            end
+        when "5"
+            if pedidos.empty?
+                puts "Nenhum pedido na fila"
+            else
+                primeiro = pedidos.first
+                puts "primeiro pedido da fila: #{primeiro}"
+            end
+        when "6"
+            if pedidos.empty?
+                puts "Nenhum pedido na fila"
+            else
+                ultimo = pedidos.last
+                puts "último pedido da fila: #{ultimo}"
+            end
+        when "7"
+            puts "Total de pedidos na fila: #{pedidos.size}"
+        when "8"
+            pedidos.clear
+            puts "Todos os pedidos foram removidos"
+        when "9"
+            puts "Fila completa de pedidos: #{pedidos.to_a}"
+        when "0"
+            puts "Fechando sistema.."
+            break
+        else
+            puts "Opção inválida, papai"
+        end
+    end
 end
