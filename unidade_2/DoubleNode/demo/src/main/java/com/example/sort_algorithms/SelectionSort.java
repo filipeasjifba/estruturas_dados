3faaf5a892aca0799f6afa1f313f536c2a025881
Filipe Alves <202421190039@ifba.edu.br>
2025-11-10 21:08:49 -0300

    feat: BubbleSort e SelectionSort adicionados
    Conteúdo visto em sala de aula

---
+++ unidade_2/DoubleNode/demo/src/main/java/com/example/sort_algorithms/SelectionSort.java
@@ -0,0 +1,29 @@
+// Aluno: Filipe Alves Sousa Julio
+
+public class SelectionSort {
+    
+    public static int[] SelectionSortEstatico (int[] array){
+        
+        for(int j = 0; j <array.length-1; j++){
+            int minorIndex = j;
+            for (int i = 1+j; i < array.length; i++) {
+                if(array[minorIndex] > array[i]){
+                    minorIndex = i;
+                }
+            }
+            int temp = array[j];
+            array[j] = array[minorIndex];
+            array[minorIndex] = temp;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,5,9,12,6,1,7};
+        SelectionSortEstatico(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+    
+}
